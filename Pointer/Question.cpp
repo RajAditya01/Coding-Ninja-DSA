75faed0e2c3f73ebbda0f21992b007c21e4c23eb
Aditya <adityavishal903@gmail.com>
2023-02-16 18:46:01 +0530

    q18

--- Pointer/Question.cpp
+++ Pointer/Question.cpp
@@ -4,5 +4,10 @@ using namespace  std;
 int main(){
     int a[5];
     int *c;
-    cout << sizeof(a) << " " << sizeof(c);    
+    cout << sizeof(a) << " " << sizeof(c)<<endl;  
+
+    //q18
+    int b[] = {1, 2, 3, 4};
+    cout << *(b) << " " << *(b+1)<<endl;
+  
 }
