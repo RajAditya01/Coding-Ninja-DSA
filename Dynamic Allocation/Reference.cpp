821e9ce8ddca685cd16aabb27bbc4bb5099c9512
Aditya <adityavishal903@gmail.com>
2023-02-19 12:32:19 +0530

    pass by value& Reference

--- Dynamic Allocation/Reference.cpp
+++ Dynamic Allocation/Reference.cpp
@@ -1,3 +1,21 @@
+//pass by reference
+#include<iostream>
+using namespace std;
+
+void increment(int& n){
+    n++;
+}
+
+int main(){
+    int i=10;
+    increment(i);
+    cout<<i<<endl;
+}
+
+/*
+
+#pass by value
+
 #include<iostream>
 using namespace std;
 
@@ -9,4 +27,4 @@ int main(){
     int i=10;
     increment(i);
     cout<<i<<endl;
-}
+}*/
