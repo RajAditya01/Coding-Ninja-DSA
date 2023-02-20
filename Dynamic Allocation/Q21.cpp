796e9b8769491500ab46f9ec563f4e530457fa83
Aditya <adityavishal903@gmail.com>
2023-02-20 22:23:26 +0530

    21

--- Dynamic Allocation/Q21.cpp
+++ Dynamic Allocation/Q21.cpp
@@ -7,3 +7,12 @@ int main(){
     cout << MULTIPLY(2+3, 3+5);
     return 0;
 }
+/*
+The macro definition uses simple text replacement to create the code that will be executed. Specifically, it replaces the macro name with the text passed as arguments, which results in the following code:
+
+cout << 2+3*3+5;
+
+This is equivalent to:
+
+cout << (2 + (3*3) + 5);
+*/
