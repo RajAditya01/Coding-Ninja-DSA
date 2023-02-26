ac9dad4cd18812634466e40ff620314763e332ed
Aditya <adityavishal903@gmail.com>
2023-02-26 11:42:39 +0530

    .

--- OPPS-2/Student1.cpp
+++ OPPS-2/Student1.cpp
@@ -1,10 +1,9 @@
 class Student {
 	int age;
+	char *name;
 
 	public :
 
-	char *name;
-	
 	Student(int age, char *name) {
 		this -> age = age;
 		// Shallow copy
@@ -14,16 +13,6 @@ class Student {
 		this -> name = new char[strlen(name) + 1];
 		strcpy(this -> name, name);
 	
-	}
-
-	// Copy constructor
-	Student(Student const &s) {
-		this -> age = s.age;
-		// this -> name = s.name;		// Shallo Copy
-	
-		// Deep copy
-		this -> name = new char[strlen(s.name) + 1];
-		strcpy(this -> name, s.name);
 	}
 
 	void display() {
