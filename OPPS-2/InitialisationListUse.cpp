41b40c73ab4e89ad7037512867797d957ba93190
Aditya <adityavishal903@gmail.com>
2023-02-26 12:13:59 +0530

    .

--- OPPS-2/InitialisationListUse.cpp
+++ OPPS-2/InitialisationListUse.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 using namespace std;
-#include "Student.cpp"
+//#include "Student.cpp"
+
+class Student {
+	public :
+	
+		int age;
+		const int rollNumber;
+		int &x;		// age reference variable
+
+		
+		
+		Student(int r, int age) : rollNumber(r), age(age), x(this -> age) {
+			//rollNumber = r;
+			
+		}
+
+		
+};
+
+
+
 
 int main() {
 	Student s1(101, 20);
 	s1.age = 20;
 	//s1.rollNumber = 101;
-	
-
 
 }
 
