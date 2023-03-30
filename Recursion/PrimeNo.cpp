fd1b6b0a1543fca34cf187f9e12fa5aeaed64161
Aditya <adityavishal903@gmail.com>
2023-03-30 13:12:51 +0530

    Prime number

--- Recursion/PrimeNo.cpp
+++ Recursion/PrimeNo.cpp
@@ -1,14 +1,44 @@
+//using while loop
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int i,count =0, j;
+//     cout<<"enter the number:";
+//     cin>>i;
+//     j=1;
+
+//     while(j<=i){
+//         if(i%j==0){
+//             count ++;
+//         }
+//         j++;
+//     }
+
+//     if(count==2){
+//         cout<<"prime Number"<<endl;
+//             }
+//     else{
+//         cout<<"Composite number"<<endl;
+//     }
+//     return 0;
+// }
+
+
+//using for loop
 #include<iostream>
 using namespace std;
 
 int main(){
-
-    int i,count =0, j;
-    cout<<"enter the number:"<<endl;
+    int i,j=1,count=0;
+    cout<<"emter the number: ";
     cin>>i;
-    j=1;
 
-    while(j<=i){
+
+
+    for(j; j<=i; j++){
         if(i%j==0){
             count ++;
         }
